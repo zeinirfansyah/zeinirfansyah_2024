7a59ea19fde001c73170a39f5e6be3c2bca429ff
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-04-05 13:50:11 +0700

    feat [about]: added about section at Home page

---
+++ src/components/home/About.jsx
@@ -0,0 +1,37 @@
+import foto2 from "../../assets/images/zein_sidang.jpg";
+
+export const About = () => {
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-12">
+      <div className="flex flex-col lg:flex-row gap-10 items-center">
+        <div
+          id="about-img"
+          className="flex justify-center items-center w-full lg:w-1/3"
+        >
+          <div className="w-1/2 lg:w-3/4 p-6 shadow-xl outline-dashed outline-2 outline-blueMunsell -outline-offset-8 lg:-rotate-3 lg:hover:rotate-0 transition-all duration-500">
+            <img src={foto2} alt="zein 2023" className="" />
+          </div>
+        </div>
+        <div id="about-text" className="flex flex-col gap-2 w-full lg:w-2/3">
+          <h1 className="text-xl lg:text-2xl font-semibold text-blueAirForce">
+            BACKGROUND
+          </h1>
+          <hr className="border border-blueAirForce" />
+          <p className="text-l lg:text-xl font-light">
+            Fresh graduate of Informatics Departement from the Universitas
+            Teknologi Yogyakarta with a GPA of 3.78 and 1 year of experience in
+            web development.
+          </p>
+          <p className="text-l lg:text-xl font-light">
+            Possesses a high level of enthusiasm and skills in software
+            engineering and digital technology honed through various software
+            development projects. Specializes in web development using Laravel
+            and React JS. Has good discipline, high loyalty, and integrity.
+            Highly motivated to work anywhere and capable of working under
+            pressure.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
+import { About } from "../components/home/About";
 import { Hero } from "../components/home/Hero";
 
 export default function Home() {
   return (
     <>
-      <div className="Home bg-antiFlashWhite">
-        <Hero />
+      <div className="Home">
+        <div className="Hero bg-antiFlashWhite">
+          <Hero />
+        </div>
+        <div className="About bg-white">
+          <About />
+        </div>
       </div>
     </>
   );
