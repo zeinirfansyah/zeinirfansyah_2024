2e641d9f3965e7e77aca5ec58729dfec83bdfccd
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-04-06 05:10:19 +0700

    add: navbar color on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,43 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Navbar } from "./layouts/Navbar";
 import Footer from "./layouts/Footer";
 import Home from "./pages/Home";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // Import AOS CSS
+import AOS from "aos";
+import "aos/dist/aos.css"; // Import AOS CSS
 
 function App() {
+
+  function navbarScroll() {
+    // get header element
+    const navbar = document.querySelector("header");
+    if (
+      document.body.scrollTop > 10 ||
+      document.documentElement.scrollTop > 10
+    ) {
+      navbar.style.backgroundColor = "#fff"; 
+      navbar.style.transition = "0.5s";
+    } else {
+      // change class
+      navbar.style.backgroundColor = "#F2F5F7"; //antiFlashWhite
+      navbar.style.transition = "0.5s";
+    }
+  }
+
   useEffect(() => {
-    AOS.init(
-      {
-        duration: 1000,
-        easing: 'ease-in-out',
-        mirror: false,
-      }
-    );
+    window.onscroll = function () {
+      navbarScroll();
+    };
+
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      mirror: false,
+    });
   }, []);
 
   return (
     <>
       <BrowserRouter>
-        <header className="shadow">
+        <header className="shadow bg-white sticky top-0 z-10">
           <Navbar />
         </header>
 
