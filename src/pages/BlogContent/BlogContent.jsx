52143661798575d3f11983735badc8d5941bb2b0
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-05-10 09:52:23 +0700

    refactor [BlogContent.jsx]: added next and previous content

--- src/pages/BlogContent/BlogContent.jsx
+++ src/pages/BlogContent/BlogContent.jsx
@@ -1,6 +1,9 @@
-import { usePost } from '../../hooks/usePost';
+import { Link } from "react-router-dom";
+import { usePost } from "../../hooks/usePost";
+import { useBlogState } from "../../store/blogState";
 
 export const BlogContent = () => {
+  const { posts } = useBlogState();
   const post = usePost();
 
   return (
@@ -19,11 +22,11 @@ export const BlogContent = () => {
               <div className="flex flex-col">
                 <span className="text-dimGray">Author: Zein Irfansyah</span>
                 <span className="text-dimGray">
-                  {new Date(post.date).toLocaleDateString('en-US', {
-                    day: 'numeric',
-                    month: 'long',
-                    year: 'numeric',
-                    timeZone: 'Asia/Jakarta',
+                  {new Date(post.date).toLocaleDateString("en-US", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                    timeZone: "Asia/Jakarta",
                   })}
                 </span>
               </div>
@@ -35,6 +38,46 @@ export const BlogContent = () => {
               dangerouslySetInnerHTML={{ __html: post.content.rendered }}
               className="blog-content"
             />
+            <div className="flex justify-between gap-6 my-7">
+              {posts.length > 0 ? (
+                posts.map((post, index) => {
+                  const currentIndex = index;
+
+                  // Find next post index (if available)
+                  const nextPostIndex =
+                    currentIndex + 1 < posts.length ? currentIndex + 1 : null;
+
+                  // Find previous post index (if available)
+                  const prevPostIndex =
+                    currentIndex > 0 ? currentIndex - 1 : null;
+                  return (
+                    <>
+                      <div>
+                        {prevPostIndex !== null && (
+                          <Link
+                            className="btn btn-primary mr-2"
+                            to={`/blog/${posts[prevPostIndex].id}`}
+                          >
+                            Next Post
+                          </Link>
+                        )}
+
+                        {nextPostIndex !== null && (
+                          <Link
+                            className="btn btn-primary"
+                            to={`/blog/${posts[nextPostIndex].id}`}
+                          >
+                            Previous Post
+                          </Link>
+                        )}
+                      </div>
+                    </>
+                  );
+                })
+              ) : (
+                <p>No posts found.</p>
+              )}
+            </div>
           </div>
         )}
       </div>
